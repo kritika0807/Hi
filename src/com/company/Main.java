6573cd5c1bfcccec816b207a1ca98e83ed0342d1
Kritika <kritika@MBP-FQ33C0WLWN>
2022-02-10 13:16:48 +0530

    add called

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -25,6 +25,7 @@ public class Main {
     public static void main(String[] args) {
      Main obj=new Main(null);
         obj.printHello();
+        System.out.println(obj.add(5,4));
     }
 
     
